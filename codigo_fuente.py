d9b4404d9c04afbb71d3eac5b5a285b978f6eb42
Nick-2455 <nicolaspacheco2455@gmail.com>
2025-04-10 14:31:24 -0700

    1 version

---
+++ codigo_fuente.py
@@ -0,0 +1,12 @@
+# This is a sample Python file
+def calculate_sum(a, b):
+    result = a + b  # Adding two numbers
+    if result > 10:
+        return "Result is greater than 10"
+    else:
+        return "Result is less than or equal to 10"
+
+# Testing with some values
+numbers = [5, 10, 15]
+for num in numbers:
+    print(f"Processing {num}")
